c796126aa8629e3ccdbd36219ba3c087218e7036
chanuko <chanu@gontoh.net>
2020-07-14 23:46:17 +0900

    パースサンプル

--- samplesite/samplesite/spiders/example.py
+++ samplesite/samplesite/spiders/example.py
@@ -1,10 +1,22 @@
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
-class ExampleSpider(scrapy.Spider):
+class ExampleSpider(CrawlSpider):
     name = 'example'
-    allowed_domains = ['example.com']
-    start_urls = ['http://example.com/']
 
-    def parse(self, response):
+    rules = (
+            Rule(LinkExtractor(allow=r'contact/'), callback='parse_contact', follow=True),
+            )
+
+    def __init__(self, *args, **kwargs):
+        super(ExampleSpider, self).__init__(*args, **kwargs)
+
+        self.start_urls = ["https://about.mercari.com/"]
+        self.allowed_domains = ["about.mercari.com"]
+
+
+    def parse_contact(self, response):
+        print(self.url)
         pass
